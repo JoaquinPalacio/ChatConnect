c3345419d328720dff4cc498bb7d3f95949c7073
Joaquin Palacio (Notebook) <jpalaciofeijoo@gmail.com>
2025-02-27 23:00:47 -0300

    creando templates

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to ChatConnect"}
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "Home"}
+    )
