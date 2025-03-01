183f766f01c45c3d78188f94e4b8dfa3b3da6f3e
Joaquin Palacio <joaquinpalaciofeijoo.04@gmail.com>
2025-03-01 20:14:03 -0300

    Se crea funcionalidad de chat

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from typing import List
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 
 
 @app.get("/")
@@ -14,3 +30,15 @@ async def home(request: Request):
     return templates.TemplateResponse(
         "index.html", {"request": request, "title": "Home"}
     )
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Usuer x: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("User disconnected")
