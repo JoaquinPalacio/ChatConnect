b9d5ed8c8cc96a88cb3e88ad7ea5fe1374e51af4
Joaquin Palacio <joaquinpalaciofeijoo.04@gmail.com>
2025-07-08 21:26:14 -0300

    Se corrijen variables de entorno

--- utils/security.py
+++ utils/security.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = os.environ.get("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
