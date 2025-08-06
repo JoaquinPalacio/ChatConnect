c68f22729ecf20218290945bb0576079d8964ff0
Joaquin Palacio <jpalaciofeijoo@gmail.com>
2025-08-06 10:00:57 -0300

    Corrección en test de login y signup

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,24 +1,27 @@
 from fastapi.testclient import TestClient
 from main import app
+import uuid
 
 client = TestClient(app)
 
 
 def test_signup_and_login():
+    username = f"usertest_{uuid.uuid4().hex[:8]}"
+    password = "pass"
     with TestClient(app) as client:
         # Signup
         res = client.post(
             "/signup",
-            data={"name": "usertest", "password": "pass", "confirmPassword": "pass"},
-            follow_redirects=False,
+            data={"name": username, "password": password, "confirmPassword": password},
+            follow_redirects=True,
         )
-        assert res.status_code == 302
+        assert res.status_code == 302 or res.status_code == 200
 
         # Login
         res = client.post(
             "/login",
-            data={"name": "testuser", "password": "pass"},
-            follow_redirects=False,
+            data={"name": username, "password": password},
+            follow_redirects=True,
         )
-        assert res.status_code == 302
-        assert "set-cookie" in res.headers
+        assert res.status_code == 302 or res.status_code == 200
+        assert "access_token" in client.cookies
